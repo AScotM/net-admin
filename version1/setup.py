058b64892badc3665c3305aee14f17726ea87ee0
Arnoldas Skinderis <102791583+AScotM@users.noreply.github.com>
2026-09-25 15:09:43 +0300

    Modify setup.py for version 2.0.0 release
    Updated version to 2.0.0 and changed setup configuration.

--- version1/setup.py
+++ version1/setup.py
@@ -2,8 +2,13 @@ from setuptools import setup
 
 setup(
     name="net-admin",
-    version="1.0.0",
-    scripts=["net-admin.py"],
-    install_requires=["colorama"],
-    description="A tool to monitor TCP connections (IPv4 & IPv6)",
+    version="2.0.0",
+    py_modules=["net_admin"],
+    entry_points={
+        "console_scripts": [
+            "net-admin=net_admin:main",
+        ],
+    },
+    python_requires=">=3.10",
+    description="Linux TCP connection monitor using /proc/net/tcp and /proc/net/tcp6",
 )
